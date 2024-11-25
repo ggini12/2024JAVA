77c7b6f9707cc6a912cc680c9b85b0e7f71950b2
ggini12 <127376412+ggini12@users.noreply.github.com>
2024-11-25 15:12:55 +0900

    Add files via upload

---
+++ chapter6/chapter6_3.java
@@ -0,0 +1,9 @@
+package report5;
+import util.Calc;
+
+public class chapter6_3 {
+public static void main(String[] args) {
+	Calc c = new Calc(10, 20);
+	System.out.println(c.sum());
+	}
+}
---
+++ chapter6/chapter6_4.java
@@ -0,0 +1,10 @@
+package report5;
+import graphic.*;
+import component.*;
+
+public class chapter6_4 {
+	public static void main(String[] args) {
+		Shape shape = new Circle();
+		shape.draw();
+	}
+}
